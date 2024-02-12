ec16b63dfdae39f180a781760e7b56e178c0e134
Ramana Kumar <ramana@member.fsf.org>
2024-02-12 19:11:42 +0000

    Add pubkey calculation and a test
    The test fails currently, so there is a bug somewhere

--- backend/run.js
+++ backend/run.js
@@ -64,6 +64,136 @@ function lamportFromParent(sk, index) {
 const deriveChild = (sk, index) =>
   secretKeyFromSeed(lamportFromParent(sk, index))
 
+// ERC-2334
+
+const getPrefixKey = seed => {
+  const m = secretKeyFromSeed(seed)
+  const c1 = deriveChild(m, 12381)
+  return deriveChild(c1, 3600)
+}
+
+const getValidatorKeys = ({seed, prefixKey}, index) => {
+  prefixKey ??= getPrefixKey(seed)
+  const indexKey = deriveChild(prefixKey, index)
+  const withdrawal = deriveChild(indexKey, 0)
+  const signing = deriveChild(withdrawal, 0)
+  return {withdrawal, signing}
+}
+
+// Get pubkey from privkey sk
+// pubkey = sk * g1 (or just its x coordinate for compressed (48 byte) form)
+// where g1 is the conventional G1 generator of BLS12-381, see
+// https://github.com/zcash/librustzcash/blob/6e0364cd42a2b3d2b958a54771ef51a8db79dd29/pairing/src/bls12_381/README.md#generators or
+// https://github.com/paulmillr/noble-curves/blob/2f1460a4d7f31e5a61db9606266f5b9a5c659c9d/src/bls12-381.ts#L1100
+//
+// To do this multiplication, we use the naive double and add algorithm
+// For doubling and adding, we use Algorithms 8 and 9 in https://eprint.iacr.org/2015/1060.pdf
+
+const g1 = {
+  x: 3685416753713387016781088315183077757961620795782546409894578378688607592378376318836054947676345821548104185464507n,
+  y: 1339506544944476473020471379941921221584933875938349620426543736416511423956333506472724655353366534992391756441569n
+}
+
+const order = 4002409555221667393417789825735904156556882819939007885332058136124031650490837864442687629129015664037894272559787n
+const b3 = 3n * 4n
+
+const mod = (a, b) => {
+  const r = a % b
+  return r < 0n ? b + r : r
+}
+const addm = (x, y) => mod(x + y, order)
+const subm = (x, y) => mod(x - y, order)
+const mulm = (x, y) => mod(x * y, order)
+
+// Algorithm 8
+const addp = ({x: x1, y: y1, z: z1}, {x: x2, y: y2}) => {
+  let t0, t1, t2, t3, t4, x3, y3, z3
+  t0 = mulm(x1, x2) //  1
+  t1 = mulm(y1, y2) //  2
+  t3 = addm(x2, y2) //  3
+  t4 = addm(x1, y1) //  4
+  t3 = mulm(t3, t4) //  5
+  t4 = addm(t0, t1) //  6
+  t3 = subm(t3, t4) //  7
+  t4 = mulm(y2, z1) //  8
+  t4 = addm(t4, y1) //  9
+  y3 = mulm(x2, z1) // 10
+  y3 = addm(y3, x1) // 11
+  x3 = addm(t0, t0) // 12
+  t0 = addm(x3, t0) // 13
+  t2 = mulm(b3, z1) // 14
+  z3 = addm(t1, t2) // 15
+  t1 = subm(t1, t2) // 16
+  y3 = mulm(b3, y3) // 17
+  x3 = mulm(t4, y3) // 18
+  t2 = mulm(t3, t1) // 19
+  x3 = subm(t2, x3) // 20
+  y3 = mulm(y3, t0) // 21
+  t1 = mulm(t1, z3) // 22
+  y3 = addm(t1, y3) // 23
+  t0 = mulm(t0, t3) // 24
+  z3 = mulm(z3, t4) // 25
+  z3 = addm(z3, t0) // 26
+  return {x: x3, y: y3, z: z3}
+}
+
+// Algorithm 9
+const dblp = ({x, y, z}) => {
+  let t0, t1, t2, x3, y3, z3
+  t0 = mulm(y, y)    //  1
+  z3 = addm(t0, t0)  //  2
+  z3 = addm(z3, z3)  //  3
+  z3 = addm(z3, z3)  //  4
+  t1 = mulm(y, z)    //  5
+  t2 = mulm(z, z)    //  6
+  t2 = mulm(b3, t2)  //  7
+  x3 = mulm(t2, z3)  //  8
+  y3 = addm(t0, t2)  //  9
+  z3 = mulm(t1, z3)  // 10
+  t1 = addm(t2, t2)  // 11
+  t2 = addm(t1, t2)  // 12
+  t0 = subm(t0, t2)  // 13
+  y3 = mulm(t0, y3)  // 14
+  y3 = addm(x3, y3)  // 15
+  t1 = mulm(x, y)    // 16
+  x3 = mulm(t0, t1)  // 17
+  x3 = addm(x3, x3)  // 18
+  return {x: x3, y: y3, z: z3}
+}
+
+const mulp = (n, p) => {
+  const {x, y} = p
+  const bits = []
+  while (n) {
+    bits.push(n % 2n)
+    n >>= 1n
+  }
+  let res = {x, y, z: 1n}
+  bits.pop()
+  while (bits.length) {
+    res = dblp(res)
+    if (bits.pop())
+      res = addp(res, p)
+  }
+  return res
+}
+
+// modular multiplicative inverse using extended Euclidean algorithm
+const invert = z => {
+  let t = 0n, u = 1n, r = order
+  while (z) {
+    const q = r / z
+    const m = t - q * u
+    const n = r - q * z
+    t = u, u = m, r = z, z = n
+  }
+  return t < 0n ? t + order : t
+}
+
+const affineX = ({x, z}) => mulm(x, invert(z))
+
+const pubkeyFromPrivkey = (sk) => affineX(mulp(sk, g1))
+
 // addresses are checksummed (ERC-55) hexstrings with the 0x prefix
 // pubkeys are lowercase hexstrings with the 0x prefix
 // timestamps are integers representing seconds since UNIX epoch
@@ -139,6 +269,21 @@ if (process.env.COMMAND == 'test') {
       process.exit(1)
     }
   }
+  const privkey = OS2IP([
+    21, 174, 215, 242, 174,  16,  11,  65,
+    60,  73,  41,  24, 106, 150,  80, 174,
+    41, 246, 248,  76,  46, 174, 109,  75,
+    77,  89,   1, 100, 227,  20,  60, 201
+  ])
+  const expectedPubkey = '0x8a0f14c0efe188fbace5b4a72f9e24ce6484b83d2a266837f69f748dafccfdcb12167f5427b7801367a32bf63fdf4783'
+  const pubkey = pubkeyFromPrivkey(privkey)
+  const hexPubkey = ethers.toBeHex(pubkey)
+  if (hexPubkey == expectedPubkey)
+    console.log(`Test pubkey passed`)
+  else {
+    console.error(`Test pubkey failed: Got ${pubkey} i.e. ${hexPubkey} instead of ${expectedPubkey}`)
+    process.exit(1)
+  }
   process.exit()
 }
 
@@ -156,8 +301,10 @@ if (process.env.COMMAND == 'init') {
 else if (process.env.COMMAND == 'deposit') {
   const dirPath = `db/${chainId}/${address}`
   const seed = new Uint8Array(readFileSync(`${dirPath}/seed`))
-  const sk = secretKeyFromSeed(seed)
-
+  const prefixKey = getPrefixKey(seed)
+  const {signing} = getValidatorKeys({prefixKey}, 0)
+  const pubkey = pubkeyFromPrivkey(signing)
+  console.log(`Got pubkey ${ethers.toBeHex(pubkey)} from signing key ${signing} at index 0`)
   console.error(`Not implemented yet: ${process.env.COMMAND}`)
   process.exit(1)
 }
